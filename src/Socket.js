7d38e69f3688ef7a4afb5f4b6de682ea68057217
ravishankar270 <ravi8291070359@gmail.com>
2024-06-17 11:05:19 +0530

    fixes

--- src/Socket.js
+++ src/Socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const DEVELOPMENT = true;
+const DEVELOPMENT = false;
 const URL = !DEVELOPMENT
   ? "https://chat-backend-6955.onrender.com"
   : "http://localhost:3500";
